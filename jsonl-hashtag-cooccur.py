72ba575337be3f031452eb4cade16f2900f30885
Derek Greene <derek.greene@ucd.ie>
2017-05-18 15:39:03 +0100

    Fix Python 2 & duplicate hashtag issues

--- jsonl-hashtag-cooccur.py
+++ jsonl-hashtag-cooccur.py
@@ -4,12 +4,13 @@ Generate a list of hashtag cooccurrence frequencies for one or more JSONL files,
 as retrieved from the Twitter API.
 
 Sample usage:
-python jsonl-hashtag-cooccur.py sample/sample-tweets-500.jsonl
+python jsonl-hashtag-cooccur.py sample/sample-tweets-500.jsonl -o hashtag-cooccurrences.csv
 """
-import sys, fileinput, itertools, io
+import sys, fileinput, itertools, operator, codecs
 from collections import defaultdict
 from optparse import OptionParser
 import logging as log
+from prettytable import PrettyTable
 try:
 	import ujson as json 
 except:
@@ -19,7 +20,7 @@ except:
 
 def main():
 	parser = OptionParser(usage="usage: %prog [options] json_file1 json_file2 ...")
-	parser.add_option("-t", "--top", action="store", type="int", dest="top", help="number of top hashtags to display", default=10)
+	parser.add_option("-t", "--top", action="store", type="int", dest="top", help="number of top pairs to display", default=10)
 	parser.add_option("-o", action="store", type="string", dest="out_path", help="output path", default="hashtag-cooccurrences.csv")
 	(options, args) = parser.parse_args()	
 	if( len(args) < 1 ):
@@ -41,12 +42,14 @@ def main():
 			try:
 				line_number += 1
 				tweet = json.loads(l)
-				tweet_tags = []
+				tweet_tags = set()
 				# find the tags
 				if "entities" in tweet:
 					if "hashtags" in tweet["entities"] and len(tweet["entities"]["hashtags"]) > 0:
 						for tag in tweet["entities"]["hashtags"]:
-							tweet_tags.append( "#" + tag["text"].lower().strip() )
+							tweet_tags.add( "#" + tag["text"].lower().strip() )
+				# do not count duplicates
+				tweet_tags = list(tweet_tags)
 				# process the pairs
 				if len(tweet_tags) > 1:
 					num_multiple += 1
@@ -69,13 +72,28 @@ def main():
 
 	# Output pairs
 	log.info("Writing pairs to %s ..." % options.out_path )
-	fout = io.open( options.out_path, "w", encoding="utf-8", errors="ignore" )
+	fout = codecs.open( options.out_path, "w", encoding="utf-8", errors="ignore" )
 	fout.write("Hashtag1\tHastag2\tCount\n")
 	for p in pair_counts:
 		pair = list(p)
 		pair.sort()
 		fout.write( "%s\t%s\t%d\n" % ( pair[0], pair[1], pair_counts[p] )  )
 	fout.close()
+
+	# Display top counts
+	sx = sorted(pair_counts.items(), key=operator.itemgetter(1), reverse=True)
+	log.info("Top %d co-occurring pairs:" % min( len(sx), options.top ) )
+	tab = PrettyTable( ["Hashtag1", "Hashtag2", "Count"] )
+	tab.align["Hashtag1"] = "l"
+	tab.align["Hashtag2"] = "l"
+	tab.align["Count"] = "r"
+	for i, p in enumerate(sx):
+		if i > options.top:
+			break
+		pair = list(p[0])
+		pair.sort()
+		tab.add_row( [pair[0], pair[1], p[1]] )
+	log.info(tab)		
 
 # --------------------------------------------------------------
 
